f02dbd7974fa641b67c47552f9545035616db963
erica-clayton <erica.baertsch@gmail.com>
2023-03-11 15:56:47 -0600

    push

--- Roommates/Program.cs
+++ Roommates/Program.cs
@@ -1,4 +1,5 @@
-﻿using Roommates.Models;
+﻿using Chores.Repositories;
+using Roommates.Models;
 using Roommates.Repositories;
 using System;
 using System.Collections.Generic;
@@ -9,11 +10,12 @@ namespace Roommates
     {
         //  This is the address of the database.
         //  We define it here as a constant since it will never change.
-        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true";
+        private const string CONNECTION_STRING = @"server=localhost,1433;database=Roommates;integrated security=false;user id=sa;password=MaizeyBaer917;TrustServerCertificate=true";
 
         static void Main(string[] args)
         {
             RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+            ChoreRepository choreRepo = new ChoreRepository(CONNECTION_STRING);
             bool runProgram = true;
             while (runProgram)
             {
@@ -31,11 +33,68 @@ namespace Roommates
                         Console.ReadKey();
                         break;
                     case ("Search for room"):
-                        // Do stuff
+                        Console.Write("Room Id: ");
+                        int id = int.Parse(Console.ReadLine());
+
+                        Room room = roomRepo.GetById(id);
+
+                        Console.WriteLine($"{room.Id} - {room.Name} Max Occupancy({room.MaxOccupancy})");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
                         break;
                     case ("Add a room"):
-                        // Do stuff
+                        Console.Write("Room name: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Max occupancy: ");
+                        int max = int.Parse(Console.ReadLine());
+
+                        Room roomToAdd = new Room()
+                        {
+                            Name = name,
+                            MaxOccupancy = max
+                        };
+
+                        roomRepo.Insert(roomToAdd);
+
+                        Console.WriteLine($"{roomToAdd.Name} has been added and assigned an Id of {roomToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
                         break;
+                    case ("Show all chores"):
+                        List<Chore> chores = choreRepo.GetAll();
+                        foreach (Chore c in chores)
+                        {
+                            Console.WriteLine($"{c.Name} has an Id of {c.Id}");
+                        }
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Search for chore"):
+                        Console.Write("Chore Id: ");
+                        int choreId = int.Parse(Console.ReadLine());
+
+                        Chore chore = choreRepo.GetById(choreId);
+
+                        Console.WriteLine($"{chore.Id} - {chore.Name}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Add a chore"):
+                        Console.Write("Chore name: ");
+                        string choreName = Console.ReadLine();
+                        Chore choreToAdd = new Chore()
+                        {
+                            Name = choreName
+                        };
+
+                        choreRepo.Insert(choreToAdd);
+
+                        Console.WriteLine($"{choreToAdd} has been assigned an Id of {choreToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
                     case ("Exit"):
                         runProgram = false;
                         break;
@@ -53,6 +112,9 @@ namespace Roommates
                 "Show all rooms",
                 "Search for room",
                 "Add a room",
+                "Show all chores",
+                "Search for chore",
+                "Add a chore",
                 "Exit"
             };
 
---
+++ Roommates/Repositories/RommateRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+using Roommates.Repositories;
+
+namespace Roommates.Repositories
+{
+	public class RommateRepository : BaseRepository
+	{
+		public RommateRepository()
+		{
+		}
+	}
+}
+
